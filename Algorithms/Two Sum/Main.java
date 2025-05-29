24c7fd167524a285a92172e9c2ee7a4032de6b54
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-05-29 12:52:12 +0530

    Implement Two Sum solution and main execution logic

---
+++ Algorithms/Two Sum/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+       
+        System.out.println("Indices of the two numbers that add up to " + target + ": [" + result[0] + ", " + result[1] + "]");
+    }
+    
+}
