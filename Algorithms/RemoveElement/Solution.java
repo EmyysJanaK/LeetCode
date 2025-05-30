c448cb9890772332c02670ee0eb01f1f2b86f5ec
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-05-30 23:59:25 +0530

    Refactor removeElement method to improve readability and maintainability

--- Algorithms/RemoveElement/Solution.java
+++ Algorithms/RemoveElement/Solution.java
@@ -3,6 +3,12 @@ package Algorithms.RemoveElement;
 public class Solution {
     public int removeElement(int[] nums, int val) {
         int k = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i]; // Move the non-val element to the front
+                k++; 
+            }
+        }
         return k;
     }
 }
