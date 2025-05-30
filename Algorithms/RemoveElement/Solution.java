a29b2068aedeee0e58cfc705519ba6eed84d4cd9
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-05-30 23:59:45 +0530

    Remove unnecessary comment in removeElement method for clarity

--- Algorithms/RemoveElement/Solution.java
+++ Algorithms/RemoveElement/Solution.java
@@ -5,7 +5,7 @@ public class Solution {
         int k = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i]; // Move the non-val element to the front
+                nums[k] = nums[i]; 
                 k++; 
             }
         }
