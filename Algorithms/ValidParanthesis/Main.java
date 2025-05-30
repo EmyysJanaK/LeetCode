75a40c226f05d31aa47ead9321437da98540c120
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2025-05-30 22:18:46 +0530

    Implement Valid Parenthesis solution

---
+++ Algorithms/ValidParanthesis/Main.java
@@ -0,0 +1,12 @@
+package Algorithms.ValidParanthesis;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        
+        String test1 = "()";
+     
+
+        System.out.println(solution.isValid(test1));
+    }
+}
---
+++ Algorithms/ValidParanthesis/Solution.java
@@ -0,0 +1,29 @@
+package Algorithms.ValidParanthesis;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        if (s == null || s.length() % 2 != 0) {
+            return false;
+        }
+        
+        Stack<Character> stack = new Stack<>();
+        for (char c: s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if ((c == ')' && top != '(') ||
+                    (c == '}' && top != '{') ||
+                    (c == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+
+    }
+}
