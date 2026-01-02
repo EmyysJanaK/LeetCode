823688a24bcc618067f2a264952ae76020ada538
Emyy'sJanak <129370684+EmyysJanaK@users.noreply.github.com>
2026-01-02 20:52:19 +0530

    Time: 2 ms (48.74%) | Memory: 43.5 MB (53.91%) - LeetSync

--- Algorithms/67-add-binary/add-binary.java
+++ Algorithms/67-add-binary/add-binary.java
@@ -2,10 +2,25 @@ import java.math.BigInteger;
 
 class Solution {
     public String addBinary(String a, String b) {
-        BigInteger decimalValueA = new BigInteger(a, 2);
-        BigInteger decimalValueB = new BigInteger(b, 2);
-        BigInteger sum = decimalValueA.add(decimalValueB);
-        String binarySumBig = sum.toString(2);
-        return binarySumBig;
+        StringBuilder result = new StringBuilder();
+        int i = a.length() - 1; 
+        int j = b.length() - 1; 
+        int carry = 0; 
+
+        while (i >= 0 || j >= 0 || carry > 0) { 
+            int sum = carry; //
+            if (i >= 0) {
+                sum += a.charAt(i) - '0';
+                i--;
+            }
+            if (j >= 0) {
+                sum += b.charAt(j) - '0';
+                j--;
+            }
+            result.append(sum % 2);
+            carry = sum / 2;
+        }
+
+        return result.reverse().toString();
     }
 }
