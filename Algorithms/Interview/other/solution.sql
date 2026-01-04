eeabca08b328a020e0d7d8d516f278f925f3ba5c
EmyysJanaK <janak.21@cse.mrt.ac.lk>
2026-01-05 00:23:18 +0530

    next quesion

--- Algorithms/Interview/other/solution.sql
+++ Algorithms/Interview/other/solution.sql
@@ -7,6 +7,12 @@ CREATE TABLE Employees (
     FirstName VARCHAR(50) ,
     LastName VARCHAR(50),
     HireDate DATE,
+    CompanyID INT,
+    FOREIGN KEY (CompanyID) REFERENCES Company(CompanyID)
+);
+CREATE TABLE Company (
+    CompanyID INT PRIMARY KEY,
+    CompanyName VARCHAR(100)
 );
 CREATE TABLE Products (
     ProductID INT PRIMARY KEY,
@@ -27,3 +33,16 @@ CREATE index idx_product_id on EmployeeProducts(ProductID);
 -- The Employees table stores employee details.
 -- The Products table stores product details.
 -- The EmployeeProducts table is a junction table that establishes a many-to-many relationship between employees and products.
+
+
+--You have a company table. 
+--How do you find the product names and the no. of employees who are working on those products 
+--in a company named “APPLE”, Write SQL Query for that
+
+SELECT p.ProductName, count(ep.EmployeeID) AS NumberOfEmployees
+FROM Products p
+JOIN EmployeeProducts ep ON p.ProductID = ep.ProductID
+JOIN Employees e ON ep.EmployeeID = e.EmployeeID
+JOIN Company c ON e.CompanyID = c.CompanyID
+WHERE c.CompanyName = 'APPLE'
+GROUP BY p.ProductName;
